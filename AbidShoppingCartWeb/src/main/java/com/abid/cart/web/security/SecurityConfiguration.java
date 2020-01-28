32c935709704eb74f3fd6306de909c61f70c69e5
abidhussain <abid786@gmail.com>
2020-01-28 14:53:46 +0300

    implemented database spring boot security

--- AbidShoppingCartWeb/src/main/java/com/abid/cart/web/security/SecurityConfiguration.java
+++ AbidShoppingCartWeb/src/main/java/com/abid/cart/web/security/SecurityConfiguration.java
@@ -1,36 +1,52 @@
 package com.abid.cart.web.security;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
+	@Autowired
+	UserDetailsDBService userDetailsService;
+	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/resources/**");
 	}
 
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
+     
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/home").hasAnyRole("USER", "ADMIN")
-				.antMatchers("/getEmployees").hasAnyRole("USER", "ADMIN").antMatchers("/addNewEmployee")
+		http.authorizeRequests().antMatchers("/")
 				.hasAnyRole("ADMIN").anyRequest().authenticated()
 				.and().formLogin().loginPage("/login").permitAll()
 				.and().logout().permitAll();
 
 		http.csrf().disable();
 	}
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception { 
+ 
+        // Setting Service to find User in the database.
+        // And Setting PassswordEncoder
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());     
+		//authenticationMgr.inMemoryAuthentication().withUser("admin@gmail.com").password("{noop}admin").authorities("ROLE_ADMIN");
 
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder authenticationMgr) throws Exception {
-		authenticationMgr.inMemoryAuthentication().withUser("admin@gmail.com").password("{noop}admin").authorities("ROLE_USER").and()
-				.withUser("javainuse").password("javainuse").authorities("ROLE_USER", "ROLE_ADMIN");
-	}
+    }
+ 
+ 
 
 }
